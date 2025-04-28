c2e7b4c5f735f56b7828660df6301dba998d0885
AppleJax2 <actionjax4@gmail.com>
2025-04-27 20:35:24 -0600

    Fix: Added missing constants file with ROLES definition

---
+++ backend/src/config/constants.js
@@ -0,0 +1,16 @@
+/**
+ * Application-wide constants
+ */
+
+// User roles for access control
+const ROLES = {
+  Admin: 'admin',
+  PetOwner: 'pet_owner',
+  MVSProvider: 'mvs_provider',
+  Clinic: 'clinic'
+};
+
+// Export all constants
+module.exports = {
+  ROLES
+}; 
