34b2b0a5149a4096af84a89eda6a4d5559c63d82
AppleJax2 <actionjax4@gmail.com>
2025-04-27 18:55:28 -0600

    Session 1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.scss' // Import Bootstrap theme and SCSS file
 // import './index.css' // We are using Bootstrap instead
 
 ReactDOM.createRoot(document.getElementById('root')).render(
